2760dcaada53eaa910171dbf93abcd13d988d3f5
Acciotharun1 <166792506+Acciotharun1@users.noreply.github.com>
2024-10-01 10:38:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function firstChar(text) {
   // your code here
+	 if(str===" " || str===""){
+        return "";
+    }
+    if(str!=""){
+        str=str.replace(/\s/g, "");
+        return str.charAt(0);
+    }
 }
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
+const text = prompt("Enter text:");
 alert(firstChar(text));
